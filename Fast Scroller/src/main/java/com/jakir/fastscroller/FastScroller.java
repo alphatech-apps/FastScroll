c5cb5987388d6c6c7c43700d7f0c55f65bd43096
AlphaTech Apps <alphatech.apps.bd@gmail.com>
2025-12-02 07:48:34 +0600

    2

--- Fast Scroller/src/main/java/com/jakir/fastscroller/FastScroller.java
+++ Fast Scroller/src/main/java/com/jakir/fastscroller/FastScroller.java
@@ -29,10 +29,11 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
  * - Attach to any RecyclerView (must use LinearLayoutManager)
  * - Works with common parent layouts (FrameLayout, LinearLayout, RelativeLayout, ConstraintLayout).
  * - Optional parameters (pass null to use defaults / theme values):
- * allWidthDp, marginFromEndDp, normalColor, activeColor, trackColor
+ * allWidthDp, marginFromEndDp, normalColor, activeColor, trackColor, swipeRefreshLayout
  * <p>
  * Usage:
- * FastScroller.attach(recyclerView); // defaults
+ * FastScroller.attach(recyclerView); // defaults if layout has not swipeRefreshLayout
+ * FastScroller.attach(recyclerView, swipeRefreshLayout);  // defaults if layout has swipeRefreshLayout
  * <p>
  * Or customize:
  * ........................
@@ -45,13 +46,13 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
  * swipeRefreshLayout   // swipeRefreshLayout enable/disable
  * );
  * Or ....................
-
+ * <p>
  * FastScroller.attach(recyclerView);
  * FastScroller.attach(recyclerView, null, null, null, null, null);
  * FastScroller.attach(recyclerView, swipeRefreshLayout);
  * FastScroller.attach(recyclerView, null, null, null, null, null, swipeRefreshLayout);
  * FastScroller.attach(recyclerView, null, null, null, null, Color.TRANSPARENT, swipeRefreshLayout);
-
+ * <p>
  * FastScroller.attach(recyclerView, null, null, null, null, 0x00000000);
  * FastScroller.attach(recyclerView, null, null, null, null, Color.TRANSPARENT);
  * FastScroller.attach(recyclerView, null, null, Color.rgb(0,0,250), null, Color.parseColor("#00000000"));
